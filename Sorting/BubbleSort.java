4b144d4257794d06769bd130484f255fc344ad4e
RITHISH <rithishmuthusamys@gmail.co>
2026-04-15 21:00:55 +0530

    Bubble Sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,5 +1,29 @@
-package Sorting;
-
+import java.util.*;
 public class BubbleSort {
-    
+    public void sort(int[] arr, int size) {
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for(int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        for(int index = 0; index < size; index++) {
+            arr[index] = input.nextInt();
+        }
+        BubbleSort object = new BubbleSort();
+        object.sort(arr, size);
+    }
 }
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
